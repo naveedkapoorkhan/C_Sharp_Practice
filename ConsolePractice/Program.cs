c08eb973f9a2bf38754d08d7bc43d235a39d53bd
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2025-09-15 00:26:50 +0500

    SORT ARRAY AFTER SPLIT AND WE SOLVE SOME PROBLEM BY SPLIT() AND JOIN();

--- ConsolePractice/Program.cs
+++ ConsolePractice/Program.cs
@@ -1,90 +1,113 @@
 ﻿//Learning objectives
 //In this module you will:
 
-//Sort and reverse the order of array elements.
-//Clear and resize the elements of an array.
+
 //Split a string into an array of strings or characters (chars).
 //Join array elements into a string.
 
+//Exercise - Discover Split() and Join()
+
+//string name = "naveed";
+//char[] characters=name.ToCharArray();
+
+//string[] stringArray=name.Split("");
+//Array.Reverse(stringArray);
+//Array.Reverse(characters);
+//foreach(char c in characters)
+//{
+//    Console.WriteLine(c);
+//}
+
+//foreach (string item in stringArray)
+//{
+//    Console.WriteLine(item);
+//}
 
 
+//string value = "abc123";
+//char[] valueArray = value.ToCharArray();
+//Array.Reverse(valueArray);
+//string result = new string(valueArray);
+//Console.WriteLine(result);
 
-//Sort and reverse the order of array elements.
 
-string[] pallets = ["B14", "A13", "B12", "A11"];
+//string value = "abc123";
+//char[] valueArray = value.ToCharArray();
+//Array.Reverse(valueArray);
+//// string result = new string(valueArray);
+//string result = string.Join(",", valueArray);
+//Console.WriteLine(result);
 
-Console.WriteLine("Sorted...");
-//Array.Sort(pallets);
-Array.Sort(pallets);
-foreach (var pallet in pallets)
+
+//Here are a few key points to remember when working with strings and arrays:
+
+//To create an array, use methods like ToCharArray() and Split()
+//To turn the array back into a single string, use methods like Join(), or create a new string passing in an array of char
+
+string value = "a-b-c-1-2-3";
+string[] arr=value.Split("-");
+
+int num = int.Parse(arr[5]);
+num = num + 1;
+
+arr[5] = num.ToString();
+string newValue = string.Join("-",arr);
+
+    Console.WriteLine(newValue);
+
+
+//when to use join first and when to use split first if string have ",/-" use first split otherwise make them char[] then join the split ok
+string values = "abc123";
+char[] valueArray = values.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
 {
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("Reversed...");
-//Array.Sort(pallets);
-Array.Reverse(pallets);
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
-
-
-//Clear and resize the elements of an array.
-
-
-string[] pallets1 = ["B14", "A11", "B12", "A13"];
-Console.WriteLine("");
-
-Array.Clear(pallets1, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets1.Length}");
-foreach (var pallet1 in pallets1)
-{
-    Console.WriteLine($"----{pallet1}");
-}
-
-
-
-
-
-
-string[] palletss = ["B14", "A11", "B12", "A13"];
-Console.WriteLine("");
-
-Array.Clear(palletss, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {palletss.Length}");
-foreach (var pallets3 in palletss)
-{
-    Console.WriteLine($"-- {pallets3}");
-}
-
-Console.WriteLine("");
-//Array.Resize(ref palletss, 6);
-Array.Resize(ref palletss,7);
-Console.WriteLine($"Resizing 6 ... count: {palletss.Length}");
-
-palletss[4] = "C01";
-palletss[5] = "C02";
-palletss[6] = "Naveed";
-
-
-foreach (var palletsss in palletss)
-{
-    Console.WriteLine($"-- {palletsss}");
-}
-
-Console.WriteLine("");
-//Array.Resize(ref palletss, 6);
-Array.Resize(ref palletss, 4);
-Console.WriteLine($"Resizing 6 ... count: {palletss.Length}");
-
-//Notice that calling Array.Resize() didn't eliminate the first two null elements. Rather, it removed the last three elements. Notably, last three elements were removed even though they contained string values.
-
-
-foreach (var palletsss in palletss)
-{
-    Console.WriteLine($"-- {palletsss}");
+    Console.WriteLine(item);
 }
 
 
 
+
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Step 1
+string[] message = pangram.Split(' ');
+
+//Step 2
+string[] newMessage = new string[message.Length];
+
+// Step 3
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+//Step 4
+string result1 = String.Join(" ", newMessage);
+Console.WriteLine(result1);
+
+////
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] NEWaRRAY = orderStream.Split(',');
+Array.Sort(NEWaRRAY);
+
+foreach (var item in NEWaRRAY)
+{
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
+}
