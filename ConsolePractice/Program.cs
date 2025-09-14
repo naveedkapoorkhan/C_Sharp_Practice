82f6203657587b6cd44d789d7bc660a0d7064c0c
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2025-09-14 21:47:05 +0500

    ***********Perform operations on arrays using helper methods in C#************** Sort and reverse the order of array elements. Clear and resize the elements of an array.

--- ConsolePractice/Program.cs
+++ ConsolePractice/Program.cs
@@ -1,16 +1,90 @@
-﻿int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+﻿//Learning objectives
+//In this module you will:
 
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-int result1 = Convert.ToInt32(value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+//Sort and reverse the order of array elements.
+//Clear and resize the elements of an array.
+//Split a string into an array of strings or characters (chars).
+//Join array elements into a string.
 
-// Your code here to set result2
-decimal result2 = value2 /(decimal) value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-// Your code here to set result3
-float result3 = value3 / (float)value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
+//Sort and reverse the order of array elements.
+
+string[] pallets = ["B14", "A13", "B12", "A11"];
+
+Console.WriteLine("Sorted...");
+//Array.Sort(pallets);
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("Reversed...");
+//Array.Sort(pallets);
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+
+//Clear and resize the elements of an array.
+
+
+string[] pallets1 = ["B14", "A11", "B12", "A13"];
+Console.WriteLine("");
+
+Array.Clear(pallets1, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets1.Length}");
+foreach (var pallet1 in pallets1)
+{
+    Console.WriteLine($"----{pallet1}");
+}
+
+
+
+
+
+
+string[] palletss = ["B14", "A11", "B12", "A13"];
+Console.WriteLine("");
+
+Array.Clear(palletss, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {palletss.Length}");
+foreach (var pallets3 in palletss)
+{
+    Console.WriteLine($"-- {pallets3}");
+}
+
+Console.WriteLine("");
+//Array.Resize(ref palletss, 6);
+Array.Resize(ref palletss,7);
+Console.WriteLine($"Resizing 6 ... count: {palletss.Length}");
+
+palletss[4] = "C01";
+palletss[5] = "C02";
+palletss[6] = "Naveed";
+
+
+foreach (var palletsss in palletss)
+{
+    Console.WriteLine($"-- {palletsss}");
+}
+
+Console.WriteLine("");
+//Array.Resize(ref palletss, 6);
+Array.Resize(ref palletss, 4);
+Console.WriteLine($"Resizing 6 ... count: {palletss.Length}");
+
+//Notice that calling Array.Resize() didn't eliminate the first two null elements. Rather, it removed the last three elements. Notably, last three elements were removed even though they contained string values.
+
+
+foreach (var palletsss in palletss)
+{
+    Console.WriteLine($"-- {palletsss}");
+}
+
+
+
