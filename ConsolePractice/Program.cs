d5769f464e9c349b5e52369c0f036099e111c344
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2025-09-16 09:32:05 +0500

    CHALLENGE OF mODULE 4 AND 5

--- ConsolePractice/Program.cs
+++ ConsolePractice/Program.cs
@@ -1,96 +1,68 @@
-﻿//Format alphanumeric data for presentation in C#
-//Use character escape sequences to add tabs, new lines, and Unicode characters to our strings.
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+decimal result = 0;
+string messa = "";
+decimal sum = 0m;
+for (int i = 0; i < values.Length; i++)
+{
+    bool valid = decimal.TryParse(values[i], out result);
+    if (valid)
+    {
+        sum = sum + result;
 
-//Create verbatim string literals, and escape common characters like backslash ( \ ) and double-quotes ( "" ).
+    }
+    else
+    {
+        messa = messa + values[i];
+    }
+}
 
-//Merge templates with variables using composite formatting and string interpolation.
-
-//Include various format specifiers for percentages, currency, and numbers.
-
-//Composite Formatting? uses numbered placeholders within a string. At run time, everything inside the braces is resolved to a
-//value that is also passed in based on their position.
-
-//This example of composite formatting uses a built-in method Format() on the string data type keyword.
-string first = "Hello";
-string second = "World";
-//The literal string "{0} {1}!" forms a template, parts of which are replaced at run time.
-//The token {0} is replaced by the first argument after the string template, in other words, the value of the variable first.
-//The token {1} is replaced by the second argument after the string template, in other words, the value of the variable second.
-string result=string.Format("First Name{1}\nLast Name:{0}", first,second);
-Console.WriteLine(result);
-
-
-string First = "Hello";
-string Second = "World";
-Console.WriteLine(string.Format("{0} {1} {0}", first, second));
-Console.WriteLine("{0} {0} {1}!", first, second);//string.format is overload here by writeline();
+Console.WriteLine(sum);
+//Console.WriteLine(messa);
 
 
-//String interpolation is a technique that simplifies composite formatting.
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-//Instead of using a numbered token and including the literal value
-//or variable name in a list of arguments to String.Format() or Console.WriteLine(),
-//you can just use the variable name inside of the curly braces.
+// Your code here to set result1
+int result1 = Convert.ToInt32(value1 / value2);
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-//In order for a string to be interpolated, you must prefix it with the $ directive. 
+// Your code here to set result2
+decimal result2 = (value2 / (decimal)value3);
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-Console.WriteLine($""); //its become interpolation Now
-string first1 = "Hello";
-string second1 = "World";
-Console.WriteLine($"{first1} {second1}!");
-Console.WriteLine($"{second1} {first1}!");
-Console.WriteLine($"{first1}\n         {first1} \n{first1}!");
-
-
-//Formatting currency Composite formatting and string interpolation can be used to format values for display given a specific language and culture.
-//In the following example,the :C currency format specifier is used to present the price and discount variables as currency
-
-decimal price = 123.45m;
-int discount = 50;
-Console.WriteLine($"Price: {price:C} (Save {discount:C})");
-//output::Price: $123.45 (Save $50.00)
-
-//What if you execute the previous code on a computer in France that has its Windows Display Language set to French?
-//  In that case you would see the following output.  Price: 123,45 € (Save 50,00 €)
-
-//The reason for the previous "€" output is that the string currency formatting feature is dependent on the local computer setting for culture.
-//In this context, the term "culture" refers to the country/region and language of the end user.
-//The culture code is a five character string that computers use to identify the location and language of the end user.
-//The culture code ensures certain information like dates and currency can be presented properly.
-
-
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N9} units");
-//If you're viewing this from the en-US culture, you observe the following output.
-// output  :Measurement: 123,456.7891 units four numbers after decimal because N4 en-US culture is used
+// Your code here to set result3
+float result3 = (value3 / value1);
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
 
 
 
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P2}");//Tax rate: 36.79%
-Console.WriteLine($"Tax rate: {tax:P3}");//Tax rate: 36.785%
-//If you're viewing this from the en-US culture, you observe the following output.
-//Tax rate: 36.79%
+string[] pallets = ["B14", "A11", "B12", "A13"];
+
+pallets[0] = pallets[1];
+pallets[0] = "kabeer";
+Console.WriteLine(pallets[0]);
+
+Console.WriteLine("mmmmmmmmmmmm");
+string[] b;
+b = pallets;
+foreach (var item in b)
+{
+    Console.WriteLine($"---{item}");
+}
+
+Console.WriteLine($"Before:{pallets[0]}");
+Console.WriteLine($"Before:{pallets[1]}");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"after:--{pallets[0]}");
+Console.WriteLine($"after:--{pallets[1]}");
 
 
-//Combining formatting approaches
-decimal price1 = 67.55m;
-decimal salePrice = 59.99m;
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
 
-string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price1 - salePrice), price1+price1);
-
-Console.WriteLine(yourDiscount);
-
-//All of the above in one code
-
-int invoiceNumber = 1201;
-decimal productShares = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
-
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"   Shares: {productShares:N3} Product");
-Console.WriteLine($"     Sub Total: {subtotal:C}");
-Console.WriteLine($"           Tax: {taxPercentage:P2}");
-Console.WriteLine($"     Total Billed: {total:C}");
